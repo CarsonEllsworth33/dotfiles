77ecd8517c3698d86286baa437cb2f64fc4e07f0
carson ellsworth <carson.a.ellsworth.civ@us.navy.mil>
2025-04-15 16:18:40 -0400

    add find definitions to #\Space s f

--- .lem/init.lisp
+++ .lem/init.lisp
@@ -105,7 +105,8 @@
 (define-key lem-vi-mode:*normal-keymap* "Leader s" *search-keymap*)
 ;; (undefined-key lem-vi-mode:*normal-keymap* "Space")
 (define-keys *search-keymap* 
-  ("g" 'lem/grep:grep))
+  ("g" 'lem/grep:grep)
+  ("f" 'lem/language-mode:find-definitions)) 
 
 
 ;; Quality of life keybindings for vi insert mode
